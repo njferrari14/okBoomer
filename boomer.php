3e89816a7e46c5dce0944a1530160977d3b45a55
Nick Ferrari <njferrari14@gmail.com>
2019-11-18 08:19:18 -0600

    initial commit

---
+++ boomer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="ok boomer">
+	<meta name="author" content="Nick Ferrari">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<div>
+		<h1>OK BOOMER</h1>
+	</div>
+	<div>
+		<h2>Your text has been boomerized! Welcome to the status quo!</h2>
+	</div>
+	<div>
+		<p>
+		</p>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="ok boomer">
+	<meta name="author" content="Nick Ferrari">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<div>
+		<h1>OK BOOMER</h1>
+	</div>
+	<div>
+		<h2>Enter text to be boomerized</h2>
+		<form method="post" action="boomer.html">
+			<input type="textarea" id="text">
+			<input type="submit" value="ok boomer">
+		</form>
+	</div>
+</body>
+</html>
